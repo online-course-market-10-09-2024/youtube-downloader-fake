018c32d63fa0c9a4df84eaeafafc2edac312912c
goatastronaut0212 <goatastronaut0212@outlook.com>
2024-09-22 06:42:35 +0700

    Init nix flake devshell

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "youtube-downloader-fake nix flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            nativeBuildInputs = [
+              # Development tools
+              pkgs.dbeaver-bin
+              pkgs.nodejs
+
+              # Others tools
+              pkgs.scc
+            ];
+          };
+        }
+      );
+    };
+}
