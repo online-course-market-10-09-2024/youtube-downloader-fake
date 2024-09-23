6bfc711416ab921d19a133f2b2f1a29986de34fa
goatastronaut0212 <goatastronaut0212@outlook.com>
2024-09-23 10:57:40 +0700

    Init api-chi golang

---
+++ api-chi/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+
+    "github.com/go-chi/chi/v5"
+    "github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+    r := chi.NewRouter()
+    r.Use(middleware.Logger)
+    r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+        w.Write([]byte("Hello World!"))
+    })
+    fmt.Println("Server running at port 3000")
+    http.ListenAndServe(":3000", r)
+}
